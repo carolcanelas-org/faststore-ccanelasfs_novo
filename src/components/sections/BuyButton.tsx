1c0b3f1d1efd86d7fd51eddc543f854b6dbc7bb9
caroolcanelas2 <carolina.canelas@vtex.com>
2024-07-05 13:59:11 -0300

    testes de overwride

---
+++ src/components/sections/BuyButton.tsx
@@ -0,0 +1,1 @@
+import { BuyButton } from '@faststore/ui'
--- src/components/sections/ProductDetailsWithCustomButton.tsx
+++ src/components/sections/ProductDetailsWithCustomButton.tsx
@@ -1,7 +1,9 @@
 import { ProductDetailsSection, getOverriddenSection } from '@faststore/core'
+import styles from './buy-button.module.scss'
 
 const ProductDetailsWithCustomButton = getOverriddenSection({
 Section: ProductDetailsSection,
+className: styles.buyButton,
 components: {
     BuyButton: { props: { size: "small", iconPosition: "right" } } 
 },
